410dbd4cfec9272576e84ad6166aed5515cf8cc3
edlymulyaandeslin <edlymulyaandeslin@gmail.com>
2026-05-05 01:02:33 +0700

    feat: implement register api

--- laravel/app/Http/Controllers/api/AuthenticatedController.php
+++ laravel/app/Http/Controllers/api/AuthenticatedController.php
@@ -3,13 +3,45 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+use function Pest\Laravel\json;
 
 class AuthenticatedController extends Controller
 {
     public function register(Request $request)
     {
-        // logic register
+        try {
+            $validators = Validator::make($request->all(), [
+                'name' => 'required|string',
+                'email' => 'required|email|unique:users,email',
+                'password' => 'required|string|min:8',
+                'confirm_password' => 'required|same:password',
+            ]);
+
+            if ($validators->fails()) {
+                throw new \Exception($validators->errors());
+            }
+
+            $input = $request->only(['name', 'email', 'password']);
+            $input['password'] = bcrypt($input['password']);
+
+            $user = User::create($input);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'User registered successfully',
+                'data' => $user,
+            ], 201);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'User registration failed',
+                'data' => json_decode($e->getMessage()),
+            ], 500);
+        }
     }
 
     public function login(Request $request)
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\api\AuthenticatedController;
 use App\Http\Controllers\api\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/register', [AuthenticatedController::class, 'register']);
-Route::get('/login', [AuthenticatedController::class, 'login']);
-Route::get('/logout', [AuthenticatedController::class, 'logout'])->middleware('auth:sanctum');
+Route::post('/register', [AuthenticatedController::class, 'register']);
+Route::post('/login', [AuthenticatedController::class, 'login']);
+Route::post('/logout', [AuthenticatedController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::apiResource('posts', PostController::class)->middleware('auth:sanctum');
